0458dd88366416ddc3800405a48fc492651f176b
Vladimir <sazanakov@inbox.ru>
2023-07-31 22:07:34 +0700

    2,3,6

--- src/components/PersonDetails/PersonDetails.tsx
+++ src/components/PersonDetails/PersonDetails.tsx
@@ -1,8 +1,31 @@
 import React, {useState, useEffect} from 'react';
 
 import './PersonDetails';
+import SwapiService from '../../services/SwapiServise';
 
-const PersonDetails = () => {
+const PersonDetails = (props: any) => {
+
+  const [person, setPerson] = useState(null);
+  const swapiService = new SwapiService;
+
+  function updatePerson(){
+    swapiService.getPerson(props.personId)
+        .then((person: any)=>{
+          setPerson(person);
+        })
+  }
+
+  useEffect(()=>{
+    updatePerson();
+  }, []);
+
+  const {id, name, gender, birthYear, eyeColor} = person;
+
+  if (!person){
+    return (
+      <span>Select the Person</span>
+    )
+  } else {
   return (
     <div className='person-details card'>
       <img className='person-image' src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} 
