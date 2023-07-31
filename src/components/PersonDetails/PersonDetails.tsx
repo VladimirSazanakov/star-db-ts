f42fe122c5799590a05c774078ed40cfb6beb775
Vladimir <sazanakov@inbox.ru>
2023-07-31 22:43:46 +0700

    2.3.6

--- src/components/PersonDetails/PersonDetails.tsx
+++ src/components/PersonDetails/PersonDetails.tsx
@@ -1,34 +1,44 @@
 import React, {useState, useEffect} from 'react';
 
-import './PersonDetails';
+import './PersonDetails.css';
 import SwapiService from '../../services/SwapiServise';
+import Spinner from '../Spinner';
 
 const PersonDetails = (props: any) => {
 
-  const [person, setPerson] = useState(null);
+
+  const [person, setPerson] = useState({id: 0, name: '', gender: '', birthYear: '', eyeColor: ''});
+  const [updateProcess, setUpdateProcess] = useState(false);
+
   const swapiService = new SwapiService;
 
   function updatePerson(){
-    swapiService.getPerson(props.personId)
+    const personId = props.personId + 1;
+    if(!personId){
+      return;
+    }
+    swapiService.getPerson(personId)
         .then((person: any)=>{
           setPerson(person);
+          setUpdateProcess(false);
         })
   }
 
   useEffect(()=>{
+    setUpdateProcess(true);
     updatePerson();
-  }, []);
+  }, [props]);
 
   const {id, name, gender, birthYear, eyeColor} = person;
 
-  if (!person){
+  if (updateProcess){
     return (
-      <span>Select the Person</span>
+      <Spinner />
     )
   } else {
   return (
     <div className='person-details card'>
-      <img className='person-image' src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} 
+      <img className='person-image' src={`https://starwars-visualguide.com/assets/img/characters/${props.personId+1}.jpg`} 
       alt="character img" />
       <div className='card-body'>
         <h4>{name}</h4>
